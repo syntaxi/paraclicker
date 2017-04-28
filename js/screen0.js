f993dbdc43ec942cbba1ca7b2031509a7de11b03
Quinn <quinn@jelly-snakes.com>
2017-04-28 19:15:23 +1000

    Make bug move randomly upon click

--- js/screen0.js
+++ js/screen0.js
@@ -2,7 +2,15 @@ var state;
 function onScreenLoad(){
 	state = parent.newGameState(JSON.parse(localStorage["save"]));
 }
-
+function speedBetweenTwoPoints(speed, x1, y1, x2, y2){
+	dist = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+	return t = dist/speed
+}
 function bugClicked(){
-	console.log("click");
+	var screenPanel = $('#screenPanel', window.parent.document);
+	var bug = $('#bugIcon');
+	bug.animate({
+        top: Math.random() * (screenPanel.height() - 100) + 50,
+        left: Math.random() * (screenPanel.width() - 100) + 50
+    });
 }
