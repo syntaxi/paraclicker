a5364e68bb1869a229196b9faca92e1e0b549b64
Quinn <quinn@jelly-snakes.com>
2017-05-08 01:55:24 +1000

    Add upgrades functionality Change storage to use the least amount of data Refactor js files to be neater

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var screenState = {
  * Called in the head before the body and hence screen has loaded. Used to set localStorage if needed
  */
 function preloadPage(){
-	localStorage["save"] = JSON.stringify(new GameState());
+	localStorage["save"] = JSON.stringify((new GameState()).convertToJson());
 }
 
 /**
@@ -55,66 +55,92 @@ function removeCover(internalCall) {
 		screenState.pageLoaded = true;
 	}
 }
+/***********
+ * Classes *
+ ***********/
 
 /**
  * Class that holds the whole game state and data that would need to be saved/passed around.
  */
 class GameState {
-	/**
-	 * @param {object} data Optional data to initialise the class with.
-	 */
-	constructor(data){
-		if (data){
-			this.bugs = data.bugs || 0;
-			this.bps = data.bsp || 0;
-			this.breeders = []
-			for (var i = 0; i < data.breeders.length; i++) {
-				this.breeders.push(new Breeder(data.breeders[i]));
-			}
-		} else {
-			this.bugs = 0;
-			this.bps = 0;
-			this.breeders = breederData;
-		}
+	constructor() {
+		this.bugs = 0;
+		this.clickRate = 1;
+		this.bps = 0;
+		this.breeders = breederData;
+		this.upgrades = upgradeData;
 		this.screenTotals = [9];
 	}
+	/**
+	 * Recalulates the Bps from all sources
+	 */
+	recalcBps() {
+		this.bps = 0
+		for (var i = 0; i < this.breeders.length; i++) {
+			this.bps += this.breeders[i].count * this.breeders[i].rate;
+		}
+	}
+	/**
+	 * Converts the state into a jsonable format.
+	 * @returns {object} A minimum jsonable version of this state
+	 */
+	convertToJson() {
+		var data = {}
+		data.bugs = this.bugs;
+		data.clickRate = this.clickRate;
+		data.breeders = []
+		for (var i = 0; i < this.breeders.length; i++) {
+			data.breeders.push(this.breeders[i].convertToJson());
+		}
+		data.upgrades = []
+		for (var i = 0; i < this.upgrades.length; i++) {
+			data.upgrades.push(this.upgrades[i].convertToJson());
+		}
+		return data
+	}
+	/**
+	 * Converts a json state into a new gamestate
+	 * @param {object} data The data to use to reconstruct the state
+	 */
+	updateFromJson(data) {
+		this.bugs = data.bugs;
+		this.clickRate = data.clickRate;
+		this.breeders = [];
+		for (var i = 0; i < data.breeders.length; i++) {
+			var breeder = new Breeder();
+			breeder.updateFromJson(data.breeders[i]);
+			this.breeders.push(breeder);
+		}
+		this.upgrades = [];
+		for (var i = 0; i < data.upgrades.length; i++) {
+			var upgrade = new Upgrade();
+			upgrade.updateFromJson(data.upgrades[i]);
+			this.upgrades.push(upgrade);
+		}
+		this.recalcBps();
+	}
 }
-/* Used to allow for a new GameState to be created from a screen. */
-function newGameState(data){return new GameState(data); }
 /**
  * Class that represents a purchasable breeder
  */
 class Breeder {
 	/**
-	 * @param {string | object} name Either the name of the breeder or the pure data object to use.
+	 * @param {string} name The name of the breeder.
 	 * @param {string} desc Description of the breeder.
 	 * @param {ind} baseCost Base cost of the breeder.
 	 * @param {int | float} rate The rate of the breeder.
 	 * @param {int} unlock The value at which the breeder is unlocked. Use 0 for unlocked at start.
-	 * 
 	 */
 	constructor(name, desc, baseCost, rate, unlock){
-		if (desc) {
-			this.name = name || "Default";
-			this.description = desc || "Default";
-			this.rate = rate || 0.1;
-			this.unlock = unlock || 0;
-			this.baseCost = baseCost;
-			this.isHidden =  !!unlock;
-			this.count = 0;
-			this.cost = baseCost;
-		} else {
-			this.name = name.name;
-			this.description = name.description;
-			this.rate = name.rate;
-			this.unlock = name.unlock;
-			this.baseCost = name.baseCost;
-			this.isHidden = name.isHidden;
-			this.count = name.count;
-			this.cost = name.cost;
-		}
+		this.name = name || "Default";
+		this.description = desc || "Default";
+		this.rate = rate || 0.1;
+		this.unlock = unlock || 0;
+		this.baseCost = baseCost;
+		this.isHidden =  Boolean(unlock);
+		this.count = 0;
+		this.cost = baseCost;
 	}
-	
 	/**
 	 * Attempts to buy one of the breeder.
 	 * @param {GameState} state The game state to use when purchasing the breeder.
@@ -123,70 +149,180 @@ class Breeder {
 	buy(state){
 		if (state.bugs >= this.cost) {
 			state.bugs -= this.cost;
-			state.bps += this.rate;
 			this.count++;
 			this.cost = Math.round(this.baseCost * Math.pow(1.05, this.count));
+			state.recalcBps();
 			return true;
 		}
 		return false;
 	}
+	/**
+	 * Create an object to be serialised
+	 * @return {object} A minimum data object to reconstruct this instance
+	 */
+	convertToJson() {
+		var data = {}
+		data.name = this.name;
+		data.description = this.description;
+		data.rate = this.rate;
+		data.unlock = this.unlock;
+		data.baseCost = this.baseCost;
+		data.isHidden = this.isHidden;
+		data.count = this.count;
+		data.cost = this.cost;
+		return data
+	}
+	/**
+	 * Update this instance with the json data
+	 * @param {object} data The data to update with
+	 */
+	updateFromJson(data) {
+		this.name = data.name;
+		this.description = data.description;
+		this.rate = data.rate;
+		this.unlock = data.unlock;
+		this.baseCost = data.baseCost;
+		this.isHidden = data.isHidden;
+		this.count = data.count;
+		this.cost = data.cost;
+	}
 }
-
+/**
+ * Class for all Upgrades
+ */
 class Upgrade {
-	
-	constructor(name, desc, price, unlockFunc, purchaseFunc){
-		if (desc) {
-			this.name = name;
-			this.desc = desc;
-			this.price = price;
-			this.purchaseFunc = purchaseFunc;
-			this.bought = false;
-		} else {
-			this.name = name.name;
-			this.desc = name.desc;
-			this.price = name.price;
-			this.purchaseFunc = name.purchaseFunc;
-			this.bought = name.bought;
-		}
+	/**
+	 * @param {string|object} name Either the name of the upgrade or the data to use to create one
+	 * @param {string} desc The description of the upgrade
+	 * @param {int} price The price of the upgrade
+	 * @param {object} unlockData Data passed to the unlock function
+	 * @param {object} postBuyData Data passed to the post-purchase function
+	 */
+	constructor(name, desc, price, unlockData, postBuyData){
+		this.name = name;
+		this.desc = desc;
+		this.price = price;
+		this.postBuyData = postBuyData;
+		this.unlockData = unlockData;
+		this.bought = false;
+		this.makeFunctions();
+		this.unlocked = false;
 	}
 	
+	/**
+	 * Attempt to buy the upgrade
+	 * @param {GameState} state The state to attempt to buy in 
+	 * @return {boolean} Indication of if the purchase was successful
+	 */
 	buy(state){
 		if (state.bugs >= this.price) {
 			state.bugs -= this.price;
 			this.bought = true;
-			this.purchaseFunc(state);
+			this.postBuyFunc(state);
+			state.recalcBps();
+			return true;
 		}
+		return false;
 	}
 	
-	isUnlocked(state){
-		return unlockFunc(state)
+	/**
+	 * Check if the upgrade can be unlocked
+	 * @param {GameState} state The state to attempt to unlock in
+ 	 * @return {boolean} Whether the upgrade can be unlocked
+	 */
+	canUnlock(state) {
+		this.unlocked = this.unlockFunc(state);
+		return this.unlocked;
+	}
+	
+	/**
+	 * Generates the unlock and post buy functions from data.
+	 */
+	makeFunctions() {
+		this.unlockFunc = genUnlock.apply(this, this.unlockData)
+		this.postBuyFunc = genPostBuy.apply(this, this.postBuyData)
+	}
+	/**
+	 * Create an object to be serialised
+	 * @return {object} A minimum data object to reconstruct this instance
+	 */
+	convertToJson() {
+		var data = {};
+		data.bought = this.bought;
+		data.name = this.name;
+		data.desc = this.desc;
+		if (!this.bought) {
+			data.price = this.price;
+			data.postBuyData = this.postBuyData;
+			data.unlockData = this.unlockData;
+		}
+		return data;
+	}
+	/**
+	 * Update this instance with the json data
+	 * @param {object} data The data to update with
+	 */
+	updateFromJson(data) {
+		this.bought = data.bought;
+		this.name = data.name;
+		this.desc = data.desc;
+		if (!this.bought) {
+			this.price = data.price;
+			this.postBuyData = data.postBuyData;
+			this.unlockData = data.unlockData;
+		}
+		this.makeFunctions();
 	}
 }
 
-/* Defines the base data for each breeder */
+/* Used to allow for a new GameState to be created from a screen. */
+function newGameState(data) {
+	var state = new GameState();
+	if (data) {
+		state.updateFromJson(data);
+	}
+	return state;
+}
+
+/***********************
+ * Closures? Factories *
+ ***********************/
+function genUnlock(min, index) {
+	var i = index;
+	return function (state) {return state.breeders[index].count >= min}
+}
+
+function genPostBuy(type, data, index) {
+	if (type == 1) {
+		return function (state) {state.breeders[index].rate *= data; }
+	} else {
+		return function (state) {state.breeders[index].rate += data; }
+	}
+}
+
+/********************
+ * Data and objects *
+ ********************/
+
+
 var breederData = [
 	new Breeder("One", "The first breeder", 15, 0.1),
-	new Breeder("Two","The second breeder", 100, 1),
-	new Breeder("Three","The third breeder", 1100, 8),
-	new Breeder("Four","The fourth breeder", 12000, 47, 100),
-	new Breeder("Five","The fifth breeder", 15, 0.3, 1000),
-	new Breeder("Six","The sixth breeder", 130000, 260, 10000),
-	new Breeder("Seven","The seventh breeder", 1400000, 1400, 100000),
-	new Breeder("Eight","The eighth breeder", 20000000, 7800, 1000000),
-	new Breeder("Nine","The ninth breeder", 330000000, 44000, 10000000),
-	new Breeder("Ten","The tenth breeder", 5100000000, 260000, 100000000)
+	new Breeder("Two", "The second breeder", 100, 1),
+	new Breeder("Three", "The third breeder", 1100, 8),
+	new Breeder("Four", "The fourth breeder", 12000, 47, 100),
+	new Breeder("Five", "The fifth breeder", 15, 0.3, 1000),
+	new Breeder("Six", "The sixth breeder", 130000, 260, 10000),
+	new Breeder("Seven", "The seventh breeder", 1400000, 1400, 100000),
+	new Breeder("Eight", "The eighth breeder", 20000000, 7800, 1000000),
+	new Breeder("Nine", "The ninth breeder", 330000000, 44000, 10000000),
+	new Breeder("Ten", "The tenth breeder", 5100000000, 260000, 100000000)
 ];
 
-var upgrades1 = [
-	new Upgrade("OneOne", "First upgrade for Breeder One", ),
-	new Upgrade("OneTwo", "Second upgrade for Breeder One"),
-	new Upgrade("OneThree", "Third upgrade for Breeder One"),
-	new Upgrade("OneFour", "Fourth upgrade for Breeder One"),
-	new Upgrade("OneFive", "Fifth upgrade for Breeder One"),
-	new Upgrade("OneSix", "Sixth upgrade for Breeder One"),
-	new Upgrade("OneSeven", "Seventh upgrade for Breeder One"),
-	new Upgrade("OneEight", "Eighth upgrade for Breeder One"),
-	new Upgrade("OneNine", "Ninth upgrade for Breeder One"),
-	new Upgrade("OneTen", "Tenth upgrade for Breeder One"),
-	new Upgrade("OneEleven", "Eleventh upgrade for Breeder One"),
+var upgradeData = [
+	new Upgrade("OneOne", "First upgrade for Breeder One", 100, [1, 0], [1, 2, 0]),
+	new Upgrade("OneTwo", "Second upgrade for Breeder One", 500, [1, 0], [1, 2, 0]),
+	new Upgrade("OneThree", "Third upgrade for Breeder One", 10000, [10, 0], [1, 2, 0]),
+	new Upgrade("OneFour", "Fourth upgrade for Breeder One", 100000, [20, 0], [2, 0.1, 0]),
+	new Upgrade("OneFive", "Fifth upgrade for Breeder One", 10000000, [40, 0], [2, 0.5, 0]),
+	new Upgrade("OneSix", "Sixth upgrade for Breeder One", 100000000, [80, 0], [2, 5, 0])
 ]
--- js/screen1.js
+++ js/screen1.js
@@ -11,30 +11,14 @@ function onScreenLoad(){
 	screen1.bugCount = $("#bugCount");
 	screen1.bugsPerSec = $("#bugsPerSec");
 	screen1.breederList = $("#breederList");
+	screen1.purchaseList = $("#purchaseList");
+	screen1.boughtList = $("#boughtList");
 	generateBreederList();
+	generateUpgradeList();
 	displayTotal();
 	parent.removeCover();
 	screen1.bpsInterval = setInterval(updateFromBPS, 50);
 	screen1.totalsInterval = setInterval(checkTotals, 2000);
-}
-
-/**
- * Adds the list of all buyable breeders to the html.
- * @param {array of Breeder objects} data The list of breeders to add to the purchase list.
- * 
- */
-function generateBreederList() {
-	for (var i = 0; i < 3; i++) {
-		addBreeder(i);
-	}
-}
-
-/**
- * Adds a breeder to the html list.
- * @param {int} i The index of the breeder to add.
- */
-function addBreeder(i) {
-	screen1.breederList.append(`<li class="breederLine bordered" id="breeder${i}" onclick="buyBreeder(${i})"><span class="breederName" id="breederName${i}">${state.breeders[i].name}</span><span class="breederInfo" id="breederInfo${i}">${state.breeders[i].description}</span><span class="breederCosts" id="breederCosts${i}">${state.breeders[i].cost}</span><span class="breederCount" id="breederCount${i}">${state.breeders[i].count}</span></li>`);
 }
 
 /**
@@ -55,13 +39,6 @@ function displayTotal() {
 }
 
 /**
- * Called when the main bug is clicked.
- */
-function bugClicked() {
-	updateTotal();
-}
-
-/**
  * Updates a breeders entry and recalculates the cost.
  * @param {integer} index The index of the breeder to update.
  */
@@ -70,20 +47,50 @@ function updateBreeder(index) {
 	$(`#breederCosts${index}`).html(state.breeders[index].cost);
 }
 
+
+/*******************
+ * Input Functions *
+ *******************/
+ 
 /**
  * Called when a breeder purchace button is pressed.
- * @param {integer} index The index of the breeder to buy.
+ * @param {integer} id The id of the breeder to buy.
  */
-function buyBreeder(index) {
-	if (index >= 0 && index <= 9) {
-		success = state.breeders[index].buy(state);
+function buyBreeder(id) {
+	if (id >= 0 && id < state.breeders.length) {
+		success = state.breeders[id].buy(state);
 		if (success) {
-			updateBreeder(index);
+			updateBreeder(id);
+			state.recalcBps();
 			displayTotal();
 		}
 	}
 }
 
+/**
+ * Called when the main bug is clicked.
+ */
+function bugClicked() {
+	//updateTotal(state.clickRate);
+	updateTotal(1000);
+}
+
+function buyUpgrade(id) {
+	if (id >= 0 && id < state.upgrades.length) {
+		success = state.upgrades[id].buy(state);
+		if (success) {
+			moveToBought(id);
+			state.recalcBps();
+			displayTotal();
+		}
+	}
+}
+
+
+/**********************
+ * Interval Functions *
+ **********************/
+ 
 /**
  * Called every 50ms to update the total number of bugs with the bps.
  */
@@ -94,11 +101,79 @@ function updateFromBPS() {
 /**
  * Called every 3s to check the unlock totals
  */
+ 
 function checkTotals() {
 	for (var i = 0; i < state.breeders.length; i++) {
 		if (state.breeders[i].isHidden && state.bugs >= state.breeders[i].unlock ) {
 			addBreeder(i);
 			state.breeders[i].isHidden = false;
+		}
+	}
+	for (var i = 0; i < state.upgrades.length; i++) {
+		if (!state.upgrades[i].unlocked && state.upgrades[i].canUnlock(state)) {
+			addPurchaseUpgrade(i);
+		}
+	}
+}
+
+
+/**************************
+ * HTML editing functions *
+ **************************/
+
+/**
+ * Adds a breeder to the html list.
+ * @param {int} i The index of the breeder to add.
+ */
+function addBreeder(i) {
+	screen1.breederList.append(`<li class="breederLine bordered" id="breeder${i}" onclick="buyBreeder(${i})"><span class="breederName" id="breederName${i}">${state.breeders[i].name}</span><span class="breederInfo" id="breederInfo${i}">${state.breeders[i].description}</span><span class="breederCosts" id="breederCosts${i}">${state.breeders[i].cost}</span><span class="breederCount" id="breederCount${i}">${state.breeders[i].count}</span></li>`);
+}
+
+/**
+ * Adds an upgrade to the html purchase list.
+ * @param {int} i The index of the upgrade to add.
+ */
+function addPurchaseUpgrade(i) {
+	screen1.purchaseList.append(`<div class="bordered upgradeBox" id="purchase${i}" onclick="buyUpgrade(${i})"><img class="upgradeIcon" src="/images/icons/upgradeIcon.png"></div>`);
+}
+
+/**
+ * Adds an upgrade to the html bought list and removes it from the buyable list
+ * @param {int} i The index of the upgrade to remove & add.
+ */
+function addBoughtUpgrade(i) {
+	screen1.boughtList.append(`<div class="bordered upgradeBox" id="purchase${i}" onclick="buyUpgrade(${i})"><img class="upgradeIcon" src="/images/icons/upgradeIcon.png"></div>`);
+}
+
+function moveToBought(id) {
+	$(`#purchase${id}`).remove();
+	addBoughtUpgrade(id);
+}
+	
+/*****************************
+ * List generating functions *
+ *****************************/
+
+/**
+ * Adds the list of all buyable breeders to the html.
+ */
+function generateBreederList() {
+	for (var i = 0; i < state.breeders.length; i++) {
+		if (!state.breeders[i].isHidden) {
+			addBreeder(i);
+		}
+	}
+}
+
+/**
+ * Adds the list of all upgrades to the html.
+ */
+function generateUpgradeList() {
+	for (var i = 0; i < state.upgrades.length; i++) {
+		if (state.upgrades[i].bought) {
+			addBoughtUpgrade(i);
+		} else if (state.upgrades[i].canUnlock(state)) {
+			addPurchaseUpgrade(i);
 		}
 	}
 }
